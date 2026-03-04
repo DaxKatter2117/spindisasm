ab74ae948da473908520949a457b8b999dac55de
DaxKatter2117 <daxternator2117@gmail.com>
2026-03-05 05:34:40 +0800

    ModEnv Macros Added

--- SOUND/DATA/mbank.asm
+++ SOUND/DATA/mbank.asm
@@ -6,6 +6,19 @@ ModPtr	macro loc
 	dc.w ((loc-GEMSModEnv)&$00FF)<<8|((loc-GEMSModEnv)&$FF00)>>8
 	endm
 
+gmModStart macro mod
+	BEW2LEW	\mod
+	endm
+
+gmModSect macro len,mod
+	dc.b	len
+	BEW2LEW	\mod
+	endm
+
+gmModEnd macro
+	dc.b	0
+	endm
+
 ; ---------------------------------------------------------------------------
 ; GEMS Modulation Pointers
 ; ---------------------------------------------------------------------------
@@ -21,44 +34,44 @@ GEMSModEnv:
 	ModPtr	ModEnv_05
 
 ModEnv_00:
-	dc.b	$00, $00
-	dc.b	$19, $2C, $01
-	dc.b	$43, $50, $FB
-	dc.b	0
+	gmModStart	0
+	gmModSect	25, 300
+	gmModSect	67, -1200
+	gmModEnd
 
 ModEnv_01:
-	dc.b	$00, $00
-	dc.b	$02, $00, $00
-	dc.b	$27, $38, $FF
-	dc.b	0
+	gmModStart	0
+	gmModSect	2, 0
+	gmModSect	39, -200
+	gmModEnd
 
 ModEnv_02:
-	dc.b	$00, $00
-	dc.b	$1C, $FE, $FF
-	dc.b	$38, $02, $00
-	dc.b	$38, $FE, $FF
-	dc.b	$36, $02, $00
-	dc.b	$36, $FE, $FF
-	dc.b	0
+	gmModStart	0
+	gmModSect	28, -2
+	gmModSect	56,  2
+	gmModSect	56, -2
+	gmModSect	54,  2
+	gmModSect	54, -2
+	gmModEnd
 
 ModEnv_03:
-	dc.b	$14, $00
-	dc.b	$0F, $0A, $00
-	dc.b	$13, $1D, $00
-	dc.b	$0D, $0A, $00
-	dc.b	0
+	gmModStart	20
+	gmModSect	15, 10
+	gmModSect	19, 29
+	gmModSect	13, 10
+	gmModEnd
 
 ModEnv_04:
-	dc.b	$00, $00
-	dc.b	$1C, $FE, $FF
-	dc.b	$38, $02, $00
-	dc.b	$38, $FE, $FF
-	dc.b	$38, $02, $00
-	dc.b	$38, $FE, $FF
-	dc.b	$1C, $02, $00
-	dc.b	0
+	gmModStart	0
+	gmModSect	28, -2
+	gmModSect	56, 2
+	gmModSect	56, -2
+	gmModSect	56, 2
+	gmModSect	56, -2
+	gmModSect	28, 2
+	gmModEnd
 
 ModEnv_05:
-	dc.b	$00, $00
-	dc.b	$20, $FA, $00
-	dc.b	0
+	gmModStart	0
+	gmModSect	32, 250
+	gmModEnd
--- src/macros.asm
+++ src/macros.asm
@@ -14,3 +14,7 @@ align:	macro
 pad_to_nearest_mbit: macro
 	dcb.b ($200000-($100000-*))-(*), $FF
 	endm
+
+BEW2LEW	macro val
+	dc.w (val&$00FF)<<8|(val&$FF00)>>8
+	endm
